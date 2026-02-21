0e35ef5ad92c88fe38708b25b7b381b6803318d6
KasidetDuangthong <ksididechdwngthxng@gmail.com>
2026-02-22 00:55:11 +0700

    test: add unit tests for 100% coverage

---
+++ test_funny_string.py
@@ -0,0 +1,16 @@
+import unittest
+from funny_string import funny_string
+
+class TestFunnyString(unittest.TestCase):
+    
+    def test_is_funny(self):  # ทดสอบกรณีที่เป็น Funny (เพื่อให้ Code รันไปจนจบฟังก์ชัน)
+        self.assertEqual(funny_string("acxz"), "Funny")
+         
+    def test_not_funny(self):  # ทดสอบกรณีที่ไม่เป็น Funny (เพื่อให้ Code เข้าเงื่อนไข if diff_s != diff_r)
+        self.assertEqual(funny_string("bcxz"), "Not Funny")
+        
+    def test_minimal_length(self):     # ทดสอบกรณีขอบเขต: สตริงสั้นที่สุด 2 ตัว เพื่อให้ Code รันไปจนจบฟังชัน
+        self.assertEqual(funny_string("ab"), "Funny")
+
+if __name__ == '__main__':
+    unittest.main()
